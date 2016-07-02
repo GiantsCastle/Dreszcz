1b01879f04a48c4a5335b298eae740d0597cbd83
Marcin Gołda <marcon1993@gmail.com>
2016-07-02 16:25:18 +0200

    Wstępny kontroler gry

---
+++ Dreszcz/Controllers/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dreszcz.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Index(int? paragraf)
+        {
+            if (paragraf != null)
+                return View("paragraf" + paragraf);
+            else
+                return View();
+        }
+    }
+}
