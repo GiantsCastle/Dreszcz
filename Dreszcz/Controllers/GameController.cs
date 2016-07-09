13d2f2c9fbb14852adf500c22bf60d959a5eded1
Marcin Gołda <marcon1993@gmail.com>
2016-07-09 15:44:03 +0200

    Przesyłanie postaci do widoku

--- Dreszcz/Controllers/GameController.cs
+++ Dreszcz/Controllers/GameController.cs
@@ -1,20 +1,27 @@
-﻿using System;
+﻿using Dreszcz.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNet.Identity;
 using System.Web.Mvc;
 
 namespace Dreszcz.Controllers
 {
     public class GameController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         // GET: Game
         public ActionResult Index(int? paragraf)
         {
+            var userId = User.Identity.GetUserId();
+            Character postac = db.Characters.FirstOrDefault(c => c.ApplicationUserId == userId);
+
             if (paragraf != null)
-                return View("paragraf" + paragraf);
+                return View("paragraf" + paragraf, postac);
             else
-                return View();
+                return View(postac);
         }
     }
 }
--- Dreszcz/Models/Character.cs
+++ Dreszcz/Models/Character.cs
@@ -5,14 +5,19 @@ using System.Web;
 
 namespace Dreszcz.Models
 {
+    public enum Eliksir { Zrecznosc, Wytrzymalosc, Szczescie };
+
     public class Character
     {
         public int CharacterId { get; set; }
 
         public string Login { get; set; }
 
-        public bool doorOpen { get; set; }
         //add character attributes here
+        public Eliksir eliksir { get; set; }
+
+        //Zmienne
+        public bool doorOpen { get; set; }
 
         public virtual ApplicationUser User { get; set; }
         public string ApplicationUserId { get; set; }
