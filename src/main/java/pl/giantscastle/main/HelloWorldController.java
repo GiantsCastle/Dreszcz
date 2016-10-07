829fa6f689c7e03d099c05c2c57940dd7af527af
Marcin Gołda <marcon1993@gmail.com>
2016-10-07 17:27:20 +0200

    Obsluga jsp
    HelloWorld

--- src/main/java/pl/giantscastle/main/HelloWorldController.java
+++ src/main/java/pl/giantscastle/main/HelloWorldController.java
@@ -1,14 +1,15 @@
 package pl.giantscastle.main;
 
 import org.springframework.stereotype.*;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloWorldController {
 
     @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
+    public String helloWorld(Model model) {
+        model.addAttribute("msg", "Polskie znaki ąęć!");
+        return "HelloWorld"; //nazwa jsp'a
     }
 }
