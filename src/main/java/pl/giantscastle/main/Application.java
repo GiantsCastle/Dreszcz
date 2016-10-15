d150b43011ebf3f44d5e61985aca471e9f7c4d8f
Marcin Gołda <marcon1993@gmail.com>
2016-10-15 21:13:02 +0200

    Usunięcie zbędnych importów

--- src/main/java/pl/giantscastle/main/Application.java
+++ src/main/java/pl/giantscastle/main/Application.java
@@ -1,10 +1,8 @@
 package pl.giantscastle.main;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @ComponentScan("pl.giantscastle.*")
